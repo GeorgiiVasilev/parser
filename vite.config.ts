a1779b00fb65f3b77bb6faf0e3d12a245e3523c2
Georgii <Ooshkap@gmail.com>
2024-02-28 15:15:31 -0500

    Change server endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://ec2-3-79-216-136.eu-central-1.compute.amazonaws.com:8080',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
